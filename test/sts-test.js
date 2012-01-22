5ca7efddbbe6bb3683a3d0c64a8389173bb76378
Ryan Fitzgerald <ryan@codebrewstudios.com>
2012-01-21 22:23:01 -0500

    testing invalid access keys

--- test/sts-test.js
+++ test/sts-test.js
@@ -2,18 +2,43 @@ var STS = require("../lib/sts").STS,
     should = require('should');
 
 describe('STS', function() {
-  var sts = new STS({accessKeyId : process.env.AWS_ACCEESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY});
+  var sts;
 
-  it('should get session token', function(done) {
-    sts.getSessionToken(function(err, credentials) {
-      
-      should.exist(credentials.sessionToken);
-      should.exist(credentials.secretAccessKey);
-      should.exist(credentials.accessKeyId);
-      should.exist(credentials.expiration);
-      credentials.expiration.should.be.an.instanceof(Date);
+  describe("with valid access keys", function() {
 
-      done();
+    beforeEach(function() {
+      sts = new STS({accessKeyId : process.env.AWS_ACCEESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY});
+    });
+
+    it('should get session token', function(done) {
+      sts.getSessionToken(function(err, credentials) {
+
+        should.exist(credentials.sessionToken);
+        should.exist(credentials.secretAccessKey);
+        should.exist(credentials.accessKeyId);
+        should.exist(credentials.expiration);
+        credentials.expiration.should.be.an.instanceof(Date);
+
+        done();
+      });
+    });
+
+  });
+
+  describe("with invalid access keys", function() {
+    beforeEach(function() {
+      sts = new STS({accessKeyId : "asdfasdfasdf", secretAccessKey: "asdf"});
+    });
+
+    it('should return Invalid Token Error', function(done) {
+
+      sts.getSessionToken(function(err, credentials) {
+        err.message.should.match(/InvalidClientTokenId/);
+        should.not.exist(credentials);
+
+        done();
+      });
+
     });
 
   });
