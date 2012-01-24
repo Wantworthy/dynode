2b12e1a2ab6336841198e50fdaf4fc1a460523ed
Ryan Fitzgerald <ryan@codebrewstudios.com>
2012-01-24 15:09:30 -0500

    implementing get item and generating hash and range keys

--- test/types-test.js
+++ test/types-test.js
@@ -29,11 +29,51 @@ describe('Types', function() {
       converted.should.eql({"foo":{"NS":['123', '44', '55']}});
     });
 
+    it("removes empty array from attributes", function() {
+      var converted = Types.stringify({foo : [123], bar:[]});
+
+      converted.should.eql({"foo":{"NS":['123']}});
+    });
+
     it("converts date to a string", function() {
       var date = new Date,
           converted = Types.stringify({foo : date});
 
       converted.should.eql({"foo":{"S":date.toString()}});
+    });
+
+  });
+
+  describe("toKeys", function() {
+    
+    it("accepts string as hash key", function(){
+      var keys = Types.toKeys("Blah");
+
+      keys.should.eql({"HashKeyElement":{"S":"Blah"}});
+    });
+
+    it("accepts number as hash key", function(){
+      var keys = Types.toKeys(55.55);
+
+      keys.should.eql({"HashKeyElement":{"N":"55.55"}});
+    });
+
+    it("accepts object with string hash key", function(){
+      var keys = Types.toKeys({hash : "Foo"});
+
+      keys.should.eql({"HashKeyElement":{"S":"Foo"}});
+    });
+
+    it("accepts object with number hash key", function(){
+      var keys = Types.toKeys({hash : 123});
+
+      keys.should.eql({"HashKeyElement":{"N": '123'}});
+    });
+
+    it("accepts object with hash and range key", function() {
+      var keys = Types.toKeys({hash : "baz", range: 456});
+
+      keys.should.eql({"HashKeyElement":{"S": 'baz'}, "RangeKeyElement":{"N": '456'}});
     });
 
   });
