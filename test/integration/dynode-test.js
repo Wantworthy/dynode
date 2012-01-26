34c583992db6e2b763070377cacf6642da651d3b
Ryan Fitzgerald <ryan@codebrewstudios.com>
2012-01-26 18:40:11 -0500

    expose api methods through default client

--- test/integration/dynode-test.js
+++ test/integration/dynode-test.js
@@ -1,20 +1,19 @@
-var Client = require("../../lib/dynode/client").Client,
+var dynode = require("../../lib/dynode"),
     DynamoDB = require('../test-helper'),
     util = require('utile'),
     should = require('should');
 
-describe('DynamoDB Client Integration Tests', function() {
-  var client;
+describe('Dynode Integration Tests', function() {
+
+  beforeEach(function() {
+    dynode.auth({accessKeyId : process.env.AWS_ACCEESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY});
+  });
 
   describe("Tables", function() {
 
-    before(function(){
-      client = new Client({accessKeyId : process.env.AWS_ACCEESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY});
-    });
-
     it('should list all tables', function(done) {
 
-      client.listTables({}, function(err, tables) {
+      dynode.listTables({}, function(err, tables) {
         tables.should.have.property("TableNames");
         done();
       });
@@ -23,7 +22,7 @@ describe('DynamoDB Client Integration Tests', function() {
 
     it('should describe table', function(done) {
       
-      client.describeTable("TestTable", function(err, table) {
+      dynode.describeTable("TestTable", function(err, table) {
         table.TableName.should.equal("TestTable");
         done();
       });
@@ -33,12 +32,9 @@ describe('DynamoDB Client Integration Tests', function() {
   });
 
   describe("Put Item", function() {
-    before(function() {
-      client = new Client({accessKeyId : process.env.AWS_ACCEESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY});
-    });
 
     it('should create new item', function(done) {
-      client.putItem("TestTable", {id : "Blah", foo: "Bar", num: 123, baz : ["a", "b", "c"]}, function(err, resp) {
+      dynode.putItem("TestTable", {id : "Blah", foo: "Bar", num: 123, baz : ["a", "b", "c"]}, function(err, resp) {
         done(err);   
       });
 
@@ -48,12 +44,11 @@ describe('DynamoDB Client Integration Tests', function() {
   describe("Get Item", function() {
     
     before(function(done) {
-      client = DynamoDB.client;
       DynamoDB.createProduct({id : "TestItem", foo: "Bar"}, done);
     });
 
     it('should get item', function(done) {
-      client.getItem("TestTable", "TestItem", function(err, item, meta) {
+      dynode.getItem("TestTable", "TestItem", function(err, item, meta) {
         item.should.eql({id : "TestItem", foo: "Bar"});
         meta.ConsumedCapacityUnits.should.equal(0.5);
         done(err);
@@ -65,12 +60,11 @@ describe('DynamoDB Client Integration Tests', function() {
   describe("Scan", function() {
     
     before(function(done) {
-      client = DynamoDB.client;
       DynamoDB.createProducts(DynamoDB.products, done);
     });
 
     it('should get all items', function(done) {
-      client.scan(DynamoDB.TestTable, function(err, items, meta) {
+      dynode.scan(DynamoDB.TestTable, function(err, items, meta) {
         items.should.have.length(meta.Count);
         done(err);
       });
@@ -78,7 +72,7 @@ describe('DynamoDB Client Integration Tests', function() {
     });
 
     it('should accept limit filter', function(done) {
-      client.scan(DynamoDB.TestTable,{Limit: 2}, function(err, items, meta) {
+      dynode.scan(DynamoDB.TestTable,{Limit: 2}, function(err, items, meta) {
         items.should.have.length(2);
         meta.Count.should.equal(2);
         done(err);
@@ -91,12 +85,11 @@ describe('DynamoDB Client Integration Tests', function() {
   describe("Delete Item", function() {
     
     beforeEach(function(done) {
-      client = DynamoDB.client;
       DynamoDB.createProduct({id : "DeleteMe", foo: "Bar"}, done);
     });
 
     it('should delete an item', function(done) {
-      client.deleteItem(DynamoDB.TestTable, "DeleteMe", function(err, resp) {
+      dynode.deleteItem(DynamoDB.TestTable, "DeleteMe", function(err, resp) {
         should.exist(resp.ConsumedCapacityUnits);
         done(err);
       });
@@ -104,7 +97,7 @@ describe('DynamoDB Client Integration Tests', function() {
     });
 
     it('should delete an item and return its old values', function(done) {
-      client.deleteItem(DynamoDB.TestTable, "DeleteMe", {ReturnValues: "ALL_OLD"}, function(err, resp) {
+      dynode.deleteItem(DynamoDB.TestTable, "DeleteMe", {ReturnValues: "ALL_OLD"}, function(err, resp) {
         resp.Attributes.should.eql({ foo: 'Bar', id: 'DeleteMe' });
         done(err);
       });
